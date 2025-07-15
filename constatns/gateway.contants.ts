51efb3c7b4aa271041072f712cbb9aa678412594
kalush666 <jonathan.kalush@gmail.com>
2025-07-15 10:04:21 +0300

    gateway

---
+++ constatns/gateway.contants.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_PORT: number = 3002;
+
+export const EVENTS = {
+  NEW_MESSAGE: "newMessage",
+  REPLY: "reply",
+  JOIN_CHAT: "joinChat",
+};
+
+export const CLIENT_ORIGIN = "http://localhost:4200";
+
+export const DEFAULT_PORT_ORIGIN = "http://localhost:3002";
